5ee6f2624a12c168e2e252e2f5d0a6edb628b736
Kate Novak <kje@cochleoid.com>
2023-11-11 20:18:06 -0500

    Stages Idea
    An idea for "stages" upon which you set a scene.

---
+++ src/stage/BasicStage.tsx
@@ -0,0 +1,29 @@
+import { PropsWithChildren } from "react";
+
+export default function BasicStage({ children, opt }: PropsWithChildren & { opt: boolean }) {
+  const color = opt ? 'bg-red-100' : 'bg-green-200'
+  const toColor = opt ? 'to-red-100' : 'to-green-200'
+  const fromColor = opt ? 'from-red-100' : 'from-green-200'
+
+  return <div className={`
+    block -my-20
+  `}>
+    <div className={`
+      lg:px-[calc(50%-500px)] px-2
+      block py-10
+      bg-gradient-to-b from-transparent ${ toColor }
+    `}></div>
+    <div className={`
+      lg:px-[calc(50%-500px)] px-2
+      block
+      ${ color }
+    `}>
+      { children }
+    </div>
+    <div className={`
+      lg:px-[calc(50%-500px)] px-2
+      block py-10
+      bg-gradient-to-b ${ fromColor } to-transparent
+    `}></div>
+  </div>
+}
